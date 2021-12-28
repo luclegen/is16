c3cb6807f0683ace496014851aa963fa7e313f2b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-28 11:48:38 +0700

    Create User

---
+++ server/app/models/user.rb
@@ -0,0 +1,22 @@
+require 'bcrypt'
+
+class User
+  include BCrypt
+  include CheckerHelper
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include ActiveModel::SecurePassword
+
+  field :avatar, type: String
+  field :name, type: String
+  field :surname, type: String
+  field :email, type: String
+  field :password_digest, type: String
+
+  validates :name, presence: { message: 'is required' }, format: { with: NAME_REGEX, message: "Invalid first name" }
+  validates :surname, presence: { message: 'is required' }, format: { with: SURNAME_REGEX, message: "Invalid last name" }
+  validates :email, uniqueness: true, presence: { message: 'is required' }, format: { with: EMAIL_REGEX, message: "Invalid email" }
+  validates :password, presence: { message: 'is required' }, format: { with: STRONG_PASSWORD_REGEX, message: "Please choose a stronger password. Try a mix of letters, numbers, and symbols (minimum is 8 characters)" }
+
+  has_secure_password
+end
