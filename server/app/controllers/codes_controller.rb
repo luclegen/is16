9b384bd305f1a148d578b5d8fdaac295be1ad602
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-12-28 11:46:10 +0700

    Create create

---
+++ server/app/controllers/codes_controller.rb
@@ -0,0 +1,28 @@
+require 'rest-client'
+
+class CodesController < ApplicationController
+  include GeneratorHelper
+
+  # POST /codes
+  def create
+    if @code = Code.where(email: params[:email]).first
+      @code.destroy
+    end
+
+    @code = Code.new(code_params)
+    code = gen_code
+
+    @code.code = code
+
+    if @code.save
+      render plain: (RestClient.post ENV['MAILER'], {:email => @code.email, :title => 'Verify email', :code => code}, {:Authorization => "Bearer #{ENV['TOKEN']}"}), status: :created
+    else
+      render json: @code.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def code_params
+      params.require(:code).permit(:email)
+    end
+end
