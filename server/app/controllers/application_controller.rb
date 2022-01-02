c3d9eafa71a84d0c7daa6de6339175dee707f2ee
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-02 15:53:28 +0700

    Create authorize

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  def authorize
+    warn = 'You are unauthorized to perform this action!'
+    if cookies.encrypted.signed[:token]
+      begin
+        @user = User.find(JWT.decode(cookies.encrypted.signed[:token], ENV['SECRET'], true)[0]['id'])
+      rescue => exception
+        render status: :unauthorized
+      end
+    else
+      render status: :unauthorized
+    end
+  end
 end
