f6f907a114b5e7609e8e0d5d13628176d1b9dac3
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-03 12:25:56 +0700

    Remove not

--- server/app/models/profile.rb
+++ server/app/models/profile.rb
@@ -8,7 +8,7 @@ class Profile
 
   validates :name, presence: { message: 'is required' }, format: { with: SURNAME_REGEX, message: 'Invalid Full name' }
   validates :dob, presence: { message: 'is required' }
-  validates :sex, presence: { message: 'is required' }, inclusion: { in: ['Male', 'Female', 'Other'], message: "%{value} is not a valid sex" }
+  validates :sex, presence: { message: 'is required' }, inclusion: { in: ['Male', 'Female', 'Other'], message: "%{value} is a invalid sex" }
 
   belongs_to :user, foreign_key: :_uid
 end
