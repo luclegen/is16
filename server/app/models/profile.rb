63d2b880e79bf11aa914376093b1be4c64723052
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-03 10:23:33 +0700

    Remove  =>

--- server/app/models/profile.rb
+++ server/app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :name, type: String
   field :dob, type: Date
@@ -7,7 +8,7 @@ class Profile
 
   validates :name, presence: { message: 'is required' }, format: { with: SURNAME_REGEX, message: 'Invalid Full name' }
   validates :dob, presence: { message: 'is required' }
-  validates :sex, presence: { message: 'is required' }, :inclusion => {:in => [ 'Male', 'Female', 'Other' ], message: "%{value} is not a valid sex" }
+  validates :sex, presence: { message: 'is required' }, inclusion: { in: ['Male', 'Female', 'Other'], message: "%{value} is not a valid sex" }
 
   belongs_to :user, foreign_key: :_uid
 end
