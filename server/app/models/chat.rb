16ade3496512b214f06a03b1f02bcaea42e5173d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-04 18:53:11 +0700

    Create Chat

---
+++ server/app/models/chat.rb
@@ -0,0 +1,11 @@
+class Chat
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :_mids, type: Array, default: []
+  field :_uids, type: Array, default: []
+
+  validates :_mids, length: { minimum: 1, message: 'The chat must have a minimum of messages!' }
+  validates :_uids, length: { minimum: 1, message: 'The chat must have a minimum of users!' }
+end
