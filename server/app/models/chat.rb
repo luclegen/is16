facc523449f464f3663be38cb9ccb222cae1f9d0
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-05 09:51:58 +0700

    set default: '#0000ff' for photo, create _aids, foreign_key: _cid

--- server/app/models/chat.rb
+++ server/app/models/chat.rb
@@ -2,11 +2,14 @@ class Chat
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :photo, type: String
+  field :photo, type: String, default: '#0000ff'
   field :title, type: String
   field :_mids, type: Array, default: []
   field :_uids, type: Array, default: []
+  field :_aids, type: Array, default: []
 
   validates :_mids, length: { minimum: 1, message: 'The chat must have a minimum of messages!' }
   validates :_uids, length: { minimum: 1, message: 'The chat must have a minimum of users!' }
+
+  belongs_to :user, foreign_key: _cid
 end
