48ce6ad3c0c087817d11f31a2442abbadd4a74b9
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-05 15:44:30 +0700

    Create create

---
+++ server/app/controllers/messages_controller.rb
@@ -0,0 +1,54 @@
+class MessagesController < ApplicationController
+  before_action :authorize, only: [:create]
+
+  def create
+    if params[:id]
+      begin
+        @chat = Chat.find(params[:id])
+      rescue => e
+        return render plain: 'Chat not found!', status: :not_found
+      end
+
+      if !@chat._uids.include?(@user._id)
+        return render nothing: true, status: :unauthorized
+      end
+    else
+      @chat = Chat.new
+      @chat._uids.push(@user._id)
+    end
+
+    @message = Message.new(message_params)
+    @message.user = @chat.user = @user
+    @chat._mids.push(@message._id)
+
+    if !params[:id] && params[:users].kind_of?(Array)
+      params[:users].each do |u|
+        begin
+          @receiver = User.find(u)
+          if !@chat._uids.include?(@receiver._id)
+            @chat._uids.push(@receiver._id)
+          end
+        rescue => e
+          return render plain: 'User not found!', status: :not_found
+        end
+      end
+    end
+
+    if @chat.save
+      @message.chat = @chat
+      if @message.save
+        render status: :created
+      else
+        render json: @message.errors, status: :unprocessable_entity
+      end
+    else
+      @message.destroy
+      render json: @chat.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:body)
+    end
+end
