d01a7fc1e142c5408dbf936caaa2f27685565f91
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-06 10:54:47 +0700

    Create show and add set_chat

--- server/app/controllers/chats_controller.rb
+++ server/app/controllers/chats_controller.rb
@@ -1,4 +1,38 @@
 class ChatsController < ApplicationController
+  before_action :authorize, only: [:show]
+  before_action :set_chat, only: [:show]
+
+  def show
+    @users = @chat._uids
+    render json: {
+      photo: @chat.photo,
+      title: @chat.title ? @chat.title : @users
+        .map { |u| User.find(u).name }
+        .uniq
+        .sort_by { |k, v| -k }
+        .join(', '),
+      messages: @chat._mids.map { |m|
+        begin
+          @message = Message.find(m)
+          begin
+            @sender = User.find(@message._uid)
+            { _uid: @sender._id,
+              _mid: @message._id,
+              avatar: @sender.avatar,
+              name: @sender.name,
+              unsent: @message.unsent,
+              body: @message.unsent ? (@sender._id == @user._id ? 'You' : @sender.name) + @message.body : @message.body,
+              time: @message.created_at }
+          rescue => e
+            return render plain: 'Sender not found!', status: :not_found
+          end
+        rescue => e
+          return render plain: 'Message not found!', status: :not_found
+        end
+      }
+    }
+  end
+
   private
     def set_chat
       begin
