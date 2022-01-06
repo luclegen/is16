5f410d13b3fe7dea8dc1ef34b8391484f5efdf21
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-06 15:31:22 +0700

    Create encrypt

--- server/app/helpers/crypto_helper.rb
+++ server/app/helpers/crypto_helper.rb
@@ -1,4 +1,12 @@
 module CryptoHelper
+  def encrypt(text)
+    cipher = OpenSSL::Cipher.new(ENV['SK_ALG'])
+    cipher.encrypt
+    cipher.key = Base64.decode64(ENV['SECRET'])
+    cipher.iv = Base64.decode64(ENV['VECTOR'])
+    Base64.encode64(cipher.update(text) + cipher.final)
+  end
+
   def decrypt(hash)
     decipher = OpenSSL::Cipher.new(ENV['SK_ALG'])
     decipher.decrypt
