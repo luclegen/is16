c1f6953146530632479e70b5214e2e91557e10fa
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-07 17:55:59 +0700

    Add methods to users resource

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :chats, only: [:show, :update, :destroy, :index]
   resources :messages, only: [:create, :destroy]
   resources :codes, only: [:create]
-  resources :users
+  resources :users, only: [:create, :show, :update, :index]
   resources :profiles
   post 'auth/', to: 'auth#login'
   get 'auth/', to: 'auth#available'
