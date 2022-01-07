c08dd0fd545f70786ef823a847338f77d2b45e12
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-07 17:56:42 +0700

    Remove profiles resource

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :messages, only: [:create, :destroy]
   resources :codes, only: [:create]
   resources :users, only: [:create, :show, :update, :index]
-  resources :profiles
   post 'auth/', to: 'auth#login'
   get 'auth/', to: 'auth#available'
   delete 'auth/', to: 'auth#logout'
