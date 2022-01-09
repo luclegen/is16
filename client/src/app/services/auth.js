5937c09debfc8ba8afaa993385194e26611d7038
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-09 11:50:17 +0700

    Create AuthService, login, and available

---
+++ client/src/app/services/auth.js
@@ -0,0 +1,12 @@
+import PU from '../apis/public'
+import PR from '../apis/private'
+
+const URL = `${process.env.REACT_APP_API_URL}auth/`
+
+class AuthService {
+  login = user => PU.post(URL, user)
+
+  available = email => PU.get(`${URL}?email=${email}`)
+}
+
+export default new AuthService()
