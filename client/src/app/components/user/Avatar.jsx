3e13734341c581663a01c7b07e73dec534ca251f
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-10 21:12:39 +0700

    Create Avatar

---
+++ client/src/app/components/user/Avatar.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+export default class Avatar extends Component {
+  render = () => <div className="avatar" style={{ color: '#' + '0'.repeat(6 - (parseInt('ffffff', 16) - parseInt(this.props.avatar.slice(1), 16)).toString(16).length) + (parseInt('ffffff', 16) - parseInt(this.props.avatar.slice(1), 16)).toString(16), background: this.props.avatar, width: this.props.width, height: this.props.height, fontSize: this.props.fontSize }}><strong>{this.props.name[0]}</strong></div>
+}
