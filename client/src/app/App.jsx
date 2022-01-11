b49e94808b0368d9bea195e26523f49a19ba606c
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-11 15:36:55 +0700

    Remove indexService

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -3,32 +3,16 @@ import { Component, Suspense, lazy } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import Loader from './components/others/Loader'
-import indexService from './services/index'
 
 const Home = lazy(() => import('./components/home/Home'))
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      ready: false
-    }
-  }
-
-  componentDidMount = () =>
-    indexService
-      .index()
-      .then(res => this.setState({ ready: res.status === 200 }))
-
   render = () => <BrowserRouter>
     <Header />
     <Suspense fallback={<Loader />}>
-      {this.state.ready ?
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-        : <Loader />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </Suspense>
   </BrowserRouter>
 }
