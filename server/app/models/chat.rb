b4231fe4ff622e4ad33bd5280a11738c6e7d7e08
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-16 05:43:29 +0700

    Add CryptoHelper, decrypt, and encrypt

--- server/app/models/chat.rb
+++ server/app/models/chat.rb
@@ -2,6 +2,8 @@ class Chat
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  include CryptoHelper
+
   field :photo, type: String, default: '#0000ff'
   field :title, type: String
   field :message, type: String
@@ -14,4 +16,12 @@ class Chat
   validates :_uids, length: { minimum: 1, message: 'The chat must have a minimum of users!' }
 
   belongs_to :user, foreign_key: :_uid
+
+  def message
+    decrypt(self[:message])
+  end
+
+  def message=message
+    self[:message] = encrypt(message)
+  end
 end
