22d8c78d74bc45f21c8b6847c1db1faafcd2d23d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-16 05:07:00 +0700

    Upper case messagesService

--- client/src/app/services/messages.js
+++ client/src/app/services/messages.js
@@ -2,10 +2,10 @@ import PR from '../apis/private'
 
 const URL = `${process.env.REACT_APP_API}messages/`
 
-class messagesService {
+class MessagesService {
   create = message => PR.post(URL, message)
 
   list = name => PR.get(`${URL}?name=${name}`)
 }
 
-export default new messagesService()
+export default new MessagesService()
