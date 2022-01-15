137d5c6eff574b15920813618d931f0b8a5b71b4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-15 12:33:07 +0700

    Create list

--- client/src/app/services/messages.js
+++ client/src/app/services/messages.js
@@ -4,6 +4,8 @@ const URL = `${process.env.REACT_APP_API}messages/`
 
 class messagesService {
   create = message => PR.post(URL, message)
+
+  list = name => PR.get(`${URL}?name=${name}`)
 }
 
 export default new messagesService()
