f485c56694e6d055df2ceebe5578bf17a500acd2
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-16 05:41:01 +0700

    Create delete

--- client/src/app/services/messages.js
+++ client/src/app/services/messages.js
@@ -5,6 +5,8 @@ const URL = `${process.env.REACT_APP_API}messages/`
 class MessagesService {
   create = message => PR.post(URL, message)
 
+  delete = id => PR.delete(`${URL}${id}`)
+
   list = name => PR.get(`${URL}?name=${name}`)
 }
 
