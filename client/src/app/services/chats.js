ae258badc53084408d8941dd2ef284708b4fbb91
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-16 05:06:39 +0700

    Create read

--- client/src/app/services/chats.js
+++ client/src/app/services/chats.js
@@ -2,8 +2,10 @@ import PR from '../apis/private'
 
 const URL = `${process.env.REACT_APP_API}chats/`
 
-class messagesService {
+class ChatsService {
+  read = id => PR.get(`${URL}${id}`)
+
   list = () => PR.get(URL)
 }
 
-export default new messagesService()
+export default new ChatsService()
