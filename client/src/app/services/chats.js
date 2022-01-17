0f833a0863ea87764041645eaebbd118abe73f16
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-17 17:00:04 +0700

    Create update

--- client/src/app/services/chats.js
+++ client/src/app/services/chats.js
@@ -5,6 +5,8 @@ const URL = `${process.env.REACT_APP_API}chats/`
 class ChatsService {
   read = id => PR.get(`${URL}${id}`)
 
+  update = chat => PR.put(`${URL}${chat?.id}`, chat)
+
   list = () => PR.get(URL)
 }
 
