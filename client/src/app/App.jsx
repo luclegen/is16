1686c0d0b81b6f26393145a4880f2960b772c21d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-18 21:08:09 +0700

    Add Profile

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/header/Header'
 import Loader from './components/others/Loader'
 
 const Home = lazy(() => import('./components/home/Home'))
+const Profile = lazy(() => import('./components/user/Profile'))
 
 export default class App extends Component {
   render = () => <BrowserRouter>
@@ -12,6 +13,7 @@ export default class App extends Component {
     <Suspense fallback={<Loader />}>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/:id" element={<Profile />} />
       </Routes>
     </Suspense>
   </BrowserRouter>
