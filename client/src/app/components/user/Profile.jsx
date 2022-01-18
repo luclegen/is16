70f381a9ae3ceb97a7cb438e8987b2363570e41c
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-18 21:08:04 +0700

    Create Profile

---
+++ client/src/app/components/user/Profile.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+  render = () =>
+    <section className="section-only">
+      <form className="form-only" onSubmit={this.submit}>
+        T
+      </form>
+    </section>
+}
