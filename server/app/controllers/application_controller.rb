9686876262f5cc73e67e990ee555677b7ce245ed
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-20 14:20:41 +0700

    Check id user

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -7,7 +7,13 @@ class ApplicationController < ActionController::API
 
   def authorize
     begin
-        @user = User.find(JWT.decode(request.headers[:Authorization].split(' ')[1], ENV['SECRET'], true)[0]['id'])
+      id = JWT.decode(request.headers[:Authorization].split(' ')[1], ENV['SECRET'], true)[0]['id']
+      if id == params[:id]
+        @user = User.find(id)
+        @profile = Profile.where(_uid: @user).first
+      else
+        render status: :unauthorized
+      end
     rescue => e
       render status: :unauthorized
     end
