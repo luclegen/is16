81e81a2793cfdf5163ae81d934eccc4e20b4cab6
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-20 07:33:46 +0700

    Create read

--- client/src/app/services/users.js
+++ client/src/app/services/users.js
@@ -6,6 +6,8 @@ const URL = `${process.env.REACT_APP_API}users/`
 class usersService {
   create = user => PU.post(URL, user)
 
+  read = id => PU.get(`${URL}${id}`)
+
   list = name => PR.get(`${URL}?name=${name}`)
 }
 
