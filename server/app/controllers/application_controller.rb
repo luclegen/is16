fc7a54494e48ff614610d00497f4f22f12dc45c8
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-01-20 14:34:20 +0700

    Update authorize

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -8,12 +8,13 @@ class ApplicationController < ActionController::API
   def authorize
     begin
       id = JWT.decode(request.headers[:Authorization].split(' ')[1], ENV['SECRET'], true)[0]['id']
-      if id == params[:id]
-        @user = User.find(id)
-        @profile = Profile.where(_uid: @user).first
-      else
-        render status: :unauthorized
+
+      if !params[:id].to_s.strip.empty? && id == params[:id]
+        return render status: :unauthorized
       end
+
+      @user = User.find(id)
+      @profile = Profile.where(_uid: @user).first
     rescue => e
       render status: :unauthorized
     end
