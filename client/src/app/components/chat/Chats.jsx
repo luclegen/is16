9ae89e1ba04e44992ec0e2b43e7a95000b3746e6
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-01 14:45:38 +0700

    Change helper.setQuery('id', res.data) to this.connect(res.data)

--- client/src/app/components/chat/Chats.jsx
+++ client/src/app/components/chat/Chats.jsx
@@ -187,7 +187,7 @@ export default class Chats extends Component {
           id: this.state.chat?._id?.['$oid'],
           body: this.state.message
         })
-      .then(res => helper.setQuery('id', res.data) || (this.refresh(res.data) && (this.reset() || e.target.reset() || this.connect(res.data)))))
+      .then(res => this.connect(res.data) && this.refresh(res.data) && (this.reset() || e.target.reset())))
 
   render = () => <section className="section-chats">
     <div className="col-chats">
