f52c4375551be700e451ec276b76a39d1ddab4b6
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-01 09:48:51 +0700

    Add actioncable, create and add CableApp to App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.sass';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import actionCable from 'actioncable'
+import './index.sass'
+import App from './app/App'
+import reportWebVitals from './reportWebVitals'
+
+const CableApp = {}
+
+CableApp.cable = actionCable.createConsumer(process.env.REACT_APP_CABLE)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App cableApp={CableApp} />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
