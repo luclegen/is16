3d404f285f03fb3f48f64090f02cb96e7640557d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-02 12:05:54 +0700

    Create TopChannel

---
+++ server/app/channels/top_channel.rb
@@ -0,0 +1,13 @@
+class TopChannel < ApplicationCable::Channel
+  def subscribed
+    stream_for Top.first
+  end
+
+  def received(data)
+    ChatChannel.broadcast_to(@top, { _cid: @top._cid, _uids: @top._uids })
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
