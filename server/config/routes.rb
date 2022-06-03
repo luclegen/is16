1ef45b0178d3e4a96a3a4fafdb3a5940d1bab977
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-03 14:28:33 +0700

    Wrap api namespace

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :chats, only: [:show, :update, :destroy, :index]
-  post 'chats/:id', to: 'chats#view'
-  resources :messages, only: [:create, :destroy]
-  resources :codes, only: [:create]
-  resources :users, only: [:create, :show, :update, :index]
-  post 'auth/', to: 'auth#login'
-  get 'auth/', to: 'auth#available'
-  delete 'auth/', to: 'auth#logout'
-  get '/', to: 'application#index'
+  namespace :api do
+    resources :chats, only: [:show, :update, :destroy, :index]
+    post 'chats/:id', to: 'chats#view'
+    resources :messages, only: [:create, :destroy]
+    resources :codes, only: [:create]
+    resources :users, only: [:create, :show, :update, :index]
+    post 'auth/', to: 'auth#login'
+    get 'auth/', to: 'auth#available'
+    delete 'auth/', to: 'auth#logout'
+    get '/', to: 'application#index'  
+  end
 end
