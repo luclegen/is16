b2dc579d42082724b6352a431ea24392ca5cc663
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-05 16:51:21 +0700

    Change PU to API

--- client/src/app/services/auth.js
+++ client/src/app/services/auth.js
@@ -1,11 +1,11 @@
-import PU from '../apis/public'
+import API from '../apis/api'
 
 const URL = `${process.env.NODE_ENV === 'production' ? window.location.origin + '/api' : process.env.REACT_APP_API}/auth/`
 
 class AuthService {
-  login = user => PU.post(URL, user)
+  login = user => API.post(URL, user)
 
-  available = email => PU.get(`${URL}?email=${email}`)
+  available = email => API.get(`${URL}?email=${email}`)
 }
 
 export default new AuthService()
