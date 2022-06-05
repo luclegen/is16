4589e732f6ffab9765d421adf0dfa6eeaecf3e32
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-05 17:14:42 +0700

    Add session[:token] to authorize

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -7,14 +7,18 @@ class ApplicationController < ActionController::API
 
   def authorize
     begin
-      id = JWT.decode(request.headers[:Authorization].split(' ')[1], ENV['SECRET'], true)[0]['id']
+      if session[:token]
+        id = JWT.decode(session[:token], ENV['SECRET'], true)[0]['id']
 
-      if !params[:id].to_s.strip.empty? && id == params[:id]
-        return render status: :unauthorized
+        if !params[:id].to_s.strip.empty? && id == params[:id]
+          return render status: :unauthorized
+        end
+
+        @user = User.find(id)
+        @profile = Profile.where(_uid: @user).first  
+      else
+        render status: :unauthorized
       end
-
-      @user = User.find(id)
-      @profile = Profile.where(_uid: @user).first
     rescue => e
       render status: :unauthorized
     end
