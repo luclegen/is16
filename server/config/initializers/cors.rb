778965708e614c17555d2320099677a62c8fc1b3
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-05 17:18:07 +0700

    Remove WEB and add WEB1, WEB2

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['WEB']
+    origins [ENV['WEB1'], ENV['WEB2']]
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
