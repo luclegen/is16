27d6b7286374f7bb29fab3667e4b2b12aa0efd4b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-07 14:25:58 +0700

    Merge list and read in refresh

--- client/src/app/components/chat/Chats.jsx
+++ client/src/app/components/chat/Chats.jsx
@@ -28,13 +28,11 @@ export default class Chats extends Component {
     }
   }
 
-  refresh = id =>
-    chatsService.list()
-      .then(chats => this.setState({ chats: chats.data }) || chatsService.read(id || this.state.chat?._id?.['$oid'] || null)
-        .then(chat => (!this.state.chat || chat.data?._id?.['$oid'] === helper.getQuery('id'))
-          && setTimeout(() => this.scroll())
-          && this.setState({ chat: chat.data }))
-        .catch(err => err.response?.status === 404 && (window.location.href = '/')))
+  refresh = id => chatsService.read(id || this.state.chat?._id?.['$oid'] || null)
+    .then(res => (!this.state.chat || res.data?.chat?._id?.['$oid'] === helper.getQuery('id'))
+      && setTimeout(() => this.scroll())
+      && this.setState({ chats: res.data?.chats, chat: res.data?.chat }))
+    .catch(err => err.response?.status === 404 && (window.location.href = '/'))
 
   connect = (id = helper.getQuery('id')) => helper.setQuery('id', id)
     || (this.props.cableApp.cid = this.props.cableApp.cable.subscriptions.create({
